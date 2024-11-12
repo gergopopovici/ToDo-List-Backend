87e7f3c0d7c9c3f7b5015431c78e59a3689006b9
gergopopovici <popovicigergo0330@gmail.com>
2024-11-12 09:42:59 +0200

    Add Handlebars integration for ToDo list

---
+++ pgim2289-web/src/main/java/edu/bbte/idde/pgim2289/web/servlet/ToDoHandleBars.java
@@ -0,0 +1,40 @@
+package edu.bbte.idde.pgim2289.web.servlet;
+
+import com.github.jknack.handlebars.Template;
+import edu.bbte.idde.pgim2289.backend.model.ToDo;
+import edu.bbte.idde.pgim2289.backend.repository.jdbc.ToDoJdbcDao;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@WebServlet("/webserver/handlebars")
+public class ToDoHandleBars extends HttpServlet {
+
+    private final ToDoJdbcDao toDoDao = new ToDoJdbcDao();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        Map<String, Object> model = new ConcurrentHashMap<>();
+
+        try {
+            Collection<ToDo> todos = toDoDao.findAll();
+            model.put("todos", todos);
+            Template template = HandleBarsFactory.getTemplate("todoList");
+            response.getWriter().println(template.apply(model));
+
+        } catch (IOException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("{\"error\":\"Template rendering error\"}");
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("{\"error\":\"Unexpected error\"}");
+        }
+    }
+}
