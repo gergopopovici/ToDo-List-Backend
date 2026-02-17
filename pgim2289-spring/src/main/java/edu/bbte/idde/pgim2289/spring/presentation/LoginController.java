ae0d46d2cc4cc2fa78f75ac8cf1273c97f69e4b7
gergopopovici <popovicigergo0330@gmail.com>
2026-02-17 07:08:03 +0200

    Fix: Cookie issues.

--- pgim2289-spring/src/main/java/edu/bbte/idde/pgim2289/spring/presentation/LoginController.java
+++ pgim2289-spring/src/main/java/edu/bbte/idde/pgim2289/spring/presentation/LoginController.java
@@ -4,11 +4,13 @@ import edu.bbte.idde.pgim2289.spring.dto.LoginRequest;
 import edu.bbte.idde.pgim2289.spring.exceptions.InvalidInputException;
 import edu.bbte.idde.pgim2289.spring.model.User;
 import edu.bbte.idde.pgim2289.spring.services.UserService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,14 +31,20 @@ public class LoginController {
 
     @PostMapping("/login")
     public User login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
-        Logger logger = org.slf4j.LoggerFactory.getLogger(LoginController.class);
+        Logger logger = LoggerFactory.getLogger(LoginController.class);
         User user = userService.findByUsername(loginRequest.getUsername());
+
         if (user != null && passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            Cookie cookie = new Cookie("userId", String.valueOf(user.getId()));
-            cookie.setHttpOnly(false);
-            cookie.setPath("/");
-            cookie.setMaxAge(60 * 60);
-            response.addCookie(cookie);
+            ResponseCookie cookie = ResponseCookie.from("userId", String.valueOf(user.getId()))
+                    .httpOnly(false)
+                    .secure(true)
+                    .path("/")
+                    .maxAge(3600)
+                    .sameSite("None")
+                    .build();
+
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
             logger.info("Login successful");
             return user;
         } else {
